5a3fdf6dfe93cd957053d16aaec889133b2dac9b
WYP_Yukino <149590332+wang-yp-whu@users.noreply.github.com>
2025-01-05 14:16:54 +0800

    Update sever_main.c

--- src/server/sever_main.c
+++ src/server/sever_main.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include "server.h"
+
+void recieve(MessagePacket message)
+{
+    switch(message.type)
+    {
+        case: HANDSHAKE_INIT
+            //do sth.
+            break;
+
+            //.......
+    }
+}
