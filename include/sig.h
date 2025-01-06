ddf0f93b27da5a8121090980642b7136336e0d10
plxj6666 <1930057086@qq.com>
2025-01-06 05:14:16 +0000

    add ack

--- include/sig.h
+++ include/sig.h
@@ -7,6 +7,7 @@ typedef struct {
     uint8_t type;          // 数据包类型 (1: 握手, 2: 数据传输, 3: 关闭连接)
     uint32_t sequence;     // 序列号，用于重传或排序
     uint16_t length;       // 数据负载的长度
+    uint16_t ack;       // 确认号，用于确认收到的数据包
     uint8_t payload[PAYLOAD_MAX_SIZE]; // 实际传输的数据 (加密后)
     uint8_t mac[32];       // 消息认证码 (如 HMAC-SHA256 输出为 32 字节)
 } MessagePacket;
@@ -33,7 +34,7 @@ typedef struct {
 #define CLOSE_REQUEST   4   // 关闭连接请求
 #define CLOSE_ACK       5   // 关闭连接确认
 #define ACK             6   //普通的信息
-
+#define KEY_EXCHANGE    7   //密钥交换
 
 //the following are the state of c/s
 int server_alive = 0;
